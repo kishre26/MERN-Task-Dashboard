b6aadadcf9df1db43c162543e95e2133a47d1f04
Kishore S <iamkishore2606@gmail.com>
2026-06-17 14:39:01 +0530

    Use dynamic port for deployment

--- backend/server.js
+++ backend/server.js
@@ -20,4 +20,5 @@ const authRoutes = require('./routes/auth');
 app.use('/api/tasks', taskRoutes);
 app.use('/api/auth', authRoutes);
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
